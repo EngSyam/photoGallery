66d1e5fe57873c258d301a3b1bb7f4bcc6f97a11
EngSyam <ahmedsyam802@gmail.com>
2019-12-16 03:57:32 +0300

    Cover Image Upload

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -13,6 +13,20 @@ class AlbumsController extends Controller
         return view('albums.create');
     }
     public function store(Request $request){
-
+        $this->validate($request,[
+            'name' => 'required',
+            'cover_image' => 'image|max:1999'
+        ]);
+        //Get FileName with Extention
+        $fileNameWithExt = $request->file('cover_image')->getClientOriginalName();
+        //Get just the file name
+        $fileName = pathinfo($fileNameWithExt,PATHINFO_FILENAME);
+        //Get File Extention
+        $Extension = $request->file('cover_image')->getClientOriginalExtension();
+        //create new file name
+        $fileNameToStore = $fileName.'_'.time().rand(1000,1000000).'.'.$Extension;
+        //uploading Image
+        $path = $request->file('cover_image')->storeAs('public/album_covers',$fileNameToStore);
+        return $path;
     }
 }
